581e1229c52a60b8843bb2d66f670bf9b4fbc677
Scott Fletcher <scottfletcher286@gmail.com>
2023-12-02 12:14:33 +0000

    completed working landing page with get and post requests

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+
+
+const app = express();
+const port = 3000;
+const API_URL = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,explicit&type=single";
+
+app.use(express.static("public"));
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.get("/", (req, res) =>{
+    res.render("index.ejs")
+});
+
+
+app.get("/getjoke", async (req, res) => {
+    try {
+        const randint = Math.floor(Math.random() * 3 + 1);
+        console.log(randint);
+        const response = await axios.get(API_URL);
+        console.log(response.data);
+        res.render("index.ejs", {joke: response.data.joke, category: response.data.category} )
+    }catch (error){
+        res.status(500);
+    };
+});
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}.`)
+})
